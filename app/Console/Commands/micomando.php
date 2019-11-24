ea90ce0f4197ff389b267be8f626694bf0ac320d
ivan <inge1998@hotmail.com>
2019-11-23 21:29:46 -0600

    PROYECTO CON scheduler

---
+++ app/Console/Commands/micomando.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB; 
+
+class micomando extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'micomando:hora';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Este es mi comando, borra cada hora la base de datos items';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DB::table('items')->delete(); 
+        echo"Todo listo";
+    }
+}
